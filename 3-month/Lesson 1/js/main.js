a7bbae1fdaf01e78a455ca68ea4cf0f16e29947d
Sherzadovich <ibrohimjalilov2019@gmail.com>
2022-11-30 15:58:55 +0500

    Data types String, Number, Boolean

---
+++ 3-month/Lesson 1/js/main.js
@@ -0,0 +1,75 @@
+//? Number
+// var x = 5;
+// console.log(x + 5);
+// var y = 6;
+// var hello = x + y;
+// console.log(hello);
+//? String 
+// var user = "Ibrohim";
+// var player = "user";
+// console.log(player);
+//? O'zgaruvchilar bilan ishlash
+// var x;
+// x = "Data";
+// console.log(x); //* Data chiqadi
+// x = "Ismoil";
+// console.log(x); //* Ismoil chiqadi
+
+//? Amaliyot
+// var x = 5;
+// var y = 9;
+// console.log(x + y); //* 14
+// x = 6;
+// console.log(x + y); //* 15
+// y = 16;
+// console.log(x + y); //* 22
+//? Mathematics
+//! +, -, /, *, **, %, ++, --
+//* **
+// console.log(4**3);
+
+//* %
+// console.log(10%4); //* 2
+
+//* ++ -> increment
+// var x = 1;
+// x++
+// x++
+// console.log(x);
+
+//* -- -> decrement
+// var x = 5;
+// x--
+// x--
+// x--
+// x--
+// console.log(x);
+
+//? Assignment Operators | +, -, * , /, **, %
+// var x = 5;
+// x = x + 2;
+// x += 2;
+// console.log(x);
+
+// var y = 6;
+// y = y -3;
+// y -= 3;
+// console.log(y);
+
+//? Data types => 1.Primitive vs 2.Non-primitive or Reference
+//* Primitives
+// String, Number, Boolean, Null, Undefined, BigInt, Symbol
+//Todo: String => '', "", ``,
+// var user1 = 'Ibrohim';
+// var user2 = `John`;
+// var user = "Ismoil va "+user1+" maktabga ketti.";
+// var user3 = `Abror "O'quvchi" ${user2} gap aytdi`;
+// console.log(user);
+
+//Todo: Number
+// var x = 4;
+// console.log(typeof x);
+
+//Todo: Boolean
+// var isUser = true;
+// console.log(typeof isUser);
