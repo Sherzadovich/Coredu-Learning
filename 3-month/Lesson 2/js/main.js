89219ef0a399e0aa68e0a5e9e434d16265ed7261
Sherzadovich <ibrohimjalilov2019@gmail.com>
2022-12-02 15:13:44 +0500

    Intro to Reference

---
+++ 3-month/Lesson 2/js/main.js
@@ -0,0 +1,53 @@
+//? Data types - Primitive vs Non-Primitive | Reference
+//* String 
+//*Number
+// console.log(123456789101112131415);
+//* Boolean
+// let x = 5;
+// let y = "5";
+// //? ==
+// console.log(x == y);
+// //? ===
+// console.log(x === y);
+//? > | <
+// console.log(x < y);
+//? >= | <=
+// console.log(x >= y); x=5 y=6 // false
+// console.log(x >= y); x=5 y=5 // true
+// console.log(x >= y); x=6 y=5 // true
+//* BigInt
+// console.log(BigInt(5496428136746638));
+//* Null -> Object
+// var username = null;
+// console.log(username);
+//* Undefined 
+// var username;
+// console.log(username);
+//* Symbol
+// var item = Symbol("Hello");
+// var item2 = Symbol("Hello");
+// console.log(item == item2);
+
+//? Browser methods
+//* alert
+// alert("Hello");
+//* prompt
+// var username = prompt("Ismingizni kiriting");
+// console.log(username);
+//* confirm
+// var username = confirm("Haqiqatdan o'chirasizmi?");
+// console.log(username);
+
+//? Non-Primitive data type -> Object
+var car = {
+    model: "Matiz",
+    year: 2022,
+    isBest: false,
+    radio: true,
+    owners: {
+        user1: "Ibrohim",
+        user2: "Abror",
+        user3: "Ismoil",
+    }
+};
+console.log(car);
