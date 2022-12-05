eca4cffc93e3b6aa3759cd10abf052c0561369f9
Sherzadovich <ibrohimjalilov2019@gmail.com>
2022-12-05 19:59:05 +0500

    Function Declaration

--- 3-month/Lesson 3/js/main.js
+++ 3-month/Lesson 3/js/main.js
@@ -1,6 +1,54 @@
-//* Amaliyot
-const username = prompt("Enter your name: ");
-const birthYear = prompt("Enter your birthyear: ");
+//? Object 
+// const obj = {
+//     id: "oiw0gjq9343r",
+//     name: "Tv",
+//     model: "LG K490",
+//     mark: 2341,
+//     accessories: {
+//         item1: "pult",
+//         item2: "podstavka",
+//         item3: "lalaku",
+//     }
+// }
 
-let result = 2022 - birthYear;
-alert(`Your age is ${result}`);
+// obj.hello = "Hello World";
+// console.log(obj);
+// obj["go"] = "gooooooooooooo";
+// console.log(obj);
+
+// delete obj.name
+// console.log(obj);
+
+// const key = "model";
+
+// console.log(obj.model);
+// console.log(obj[key]);
+
+
+//* Amaliyot
+// const username = prompt("Enter your name: ");
+// const birthYear = prompt("Enter your birthyear: ");
+
+// let result = 2022 - birthYear;
+// alert(`Your age is ${result}`);
+
+
+//? Type conversion vs Type coersion
+//*Type coersion  
+// console.log(2022 - "2002");
+// console.log(2022 - "hello");  // Nan => Not a Number
+// console.log(2022 + "2002");
+
+//* Type conversion
+// const num = prompt("Enter Number:");
+// console.log(String(45));
+// console.log(Number(num));
+
+//? Function Declaration
+// function sayHello() {
+//     const username = prompt("Enter your name:");
+//     const birthYear = prompt("Enter your birthyear:");
+
+//     let result = 2022 - birthYear;
+//     alert(`Your age is ${result}`);
+}
