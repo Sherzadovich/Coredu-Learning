007a6b13e336e462c797a5d6d038fcd6e25bec51
Sherzadovich <ibrohimjalilov2019@gmail.com>
2022-12-12 16:25:36 +0500

    Ternary operator

--- 3-month/Lesson 6/js/main.js
+++ 3-month/Lesson 6/js/main.js
@@ -6,3 +6,21 @@
 // } else {
 //     console.log("This is Empty value");
 // };
+
+//? Ternary operator
+// const text = prompt("Firstname");
+
+let x = 19;
+const num = x < 18 ? console.log("Hello under 18") : console.log("Hello bobo");
+// const user = text ? text : "Hurmatli mijoz";
+
+// console.log(user);
+
+// let user = null;
+
+// if(text) {
+//     user = text;
+// } else {
+//     user = "Xurmatli mijoz!"
+// }
+// console.log(user);
