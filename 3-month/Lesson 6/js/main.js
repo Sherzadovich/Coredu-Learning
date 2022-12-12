2fedb399e71ed659155399309aa2fbb119a40212
Sherzadovich <ibrohimjalilov2019@gmail.com>
2022-12-12 16:12:36 +0500

    Truethy and Falsy values

---
+++ 3-month/Lesson 6/js/main.js
@@ -0,0 +1,8 @@
+//? Truethy vs Falsy values
+//* Falsy values => "", 0, null, undefined, false
+// const text = prompt("Text");
+// if(text) {
+//     console.log(text);
+// } else {
+//     console.log("This is Empty value");
+// };
