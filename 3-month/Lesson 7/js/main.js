3a5658ea960441f2566ddf5b27a3e879e79a1563
Sherzadovich <ibrohimjalilov2019@gmail.com>
2022-12-14 19:37:39 +0500

    pop, push, shift, unshift array methods

--- 3-month/Lesson 7/js/main.js
+++ 3-month/Lesson 7/js/main.js
@@ -21,14 +21,33 @@
 // }
 
 
+//? Array methods => push, pop, unshift, shift
+const arr = [1, 2, 3];
+// arr[arr.length] = "Ibrohim";
+// arr[arr.length] = "Ismoil";
 
+//* push => add new element from end of the array
+// const add = arr.push("Ibrohim", "Isom"); //! Returns length of the array = 2
+// const add2 = arr.push("Ismoil"); //! Returns length of the array = 3
+//* pop => delete last element from end of the array
+// const deletedEl = arr.pop() //! returns deleted element
+// arr.pop() //! delete last element from array
+//* unshift => adds new element from start of the array
+// arr.unshift(8);
+// arr.unshift(9);
+// const data = arr.unshift(7); //! Returns length of the array
+//* shift => delete last element from start of the array
+// const data = arr.shift(); //!returns deleted element
+// arr.shift();
 
+// console.log(data);
+// console.log(arr);
 
 
 //* Amaliyot
-const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-function finder(array) {
-};
+// function finder(array) {
+// };
 
-finder(arr);
+// finder(arr);
