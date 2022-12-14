1dcbb74db6a5378c6dc760f6230c04bb387d96c0
Sherzadovich <ibrohimjalilov2019@gmail.com>
2022-12-14 19:07:54 +0500

    switch statement

---
+++ 3-month/Lesson 7/js/main.js
@@ -0,0 +1,34 @@
+//? Switch statement
+// const text = prompt("Enter text:");
+
+// switch(text) {
+//     case "3":
+//         console.log("Hello 3");
+//         break;
+//     case "4":
+//         console.log("Hello 4");   
+//         break;
+//     default:
+//         console.log("Hello default");
+//         break;     
+// }
+
+
+// if(text === "Ibrohim") {
+//     console.log("Hello Ibrohim");
+// } else {
+//     console.log("Hello Vs");
+// }
+
+
+
+
+
+
+//* Amaliyot
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+function finder(array) {
+};
+
+finder(arr);
