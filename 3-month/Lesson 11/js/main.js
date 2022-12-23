eb1875c7291ea94c9c1ddbaf330722219e1cf119
Sherzadovich <ibrohimjalilov2019@gmail.com>
2022-12-23 22:08:30 +0500

    Math methods vs Math random

--- 3-month/Lesson 11/js/main.js
+++ 3-month/Lesson 11/js/main.js
@@ -21,3 +21,32 @@
 //     console.log(e.results[0][0].transcript);
 //     btnEl.style.backgroundColor = colors[e.results[0][0].transcript]
 // };
+
+//? Math
+//* Math.round()
+// console.log(Math.round(4.5)); // 5
+// console.log(Math.round(4.6)); // 5
+// console.log(Math.round(4.4)); // 4
+//* Math.ceil()
+// console.log(Math.ceil(4.8)); // 5
+// console.log(Math.ceil(4.1)); // 5
+// console.log(Math.ceil(4.5)); // 5
+//* Math.floor()
+// console.log(Math.floor(4.5)); // 4
+// console.log(Math.floor(4.9)); // 4
+// console.log(Math.floor(4.3)); // 4
+// console.log(Math.floor(-4.7)); // -5
+//* Math.trunc()
+// console.log(Math.trunc(4.5)); // 4
+// console.log(Math.trunc(4.9)); // 4
+// console.log(Math.trunc(4.3)); // 4
+// console.log(Math.trunc(-4.7)); // -4
+//* Math.max() vs Math.min()
+// console.log(Math.max(1,30,-50,400,-2)); // 400
+// console.log(Math.min(1,30,-50,400,-2)); // -50
+
+//? Math.random()
+// const arr = ["Ibrohim", "Akbar", "Islom", "Ahror", "Zafar"]
+// const index = Math.floor(Math.random() * arr.length)
+
+// console.log(arr[index], index);
