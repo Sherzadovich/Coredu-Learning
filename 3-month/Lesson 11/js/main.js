ccdffae38257d23d8de6640fe99503e80f9d4de3
Sherzadovich <ibrohimjalilov2019@gmail.com>
2022-12-23 21:44:36 +0500

    webkitSpeechRecognition

---
+++ 3-month/Lesson 11/js/main.js
@@ -0,0 +1,23 @@
+//? API => Application programming interface
+// const recognition = new webkitSpeechRecognition();
+// recognition.lang = "uz-UZ";
+
+// const btnEl = document.querySelector("#btn");
+
+// btnEl.addEventListener("click", () => {
+//     recognition.start();
+// });
+
+// const colors = {
+//     qizil: "red",
+//     yashil: "Green",
+//     sariq: "Yellow",
+// }
+
+// colors.qizil
+// colors["qizil"]
+
+// recognition.onresult = (e) => {
+//     console.log(e.results[0][0].transcript);
+//     btnEl.style.backgroundColor = colors[e.results[0][0].transcript]
+// };
