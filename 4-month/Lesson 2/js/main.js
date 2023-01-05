d566b916d2ad50fad68a45f33395d7511a7265c9
Sherzadovich <ibrohimjalilov2019@gmail.com>
2023-01-06 04:39:26 +0500

    Onchange

---
+++ 4-month/Lesson 2/js/main.js
@@ -0,0 +1,10 @@
+// const radios = document.getElementsByName("gender");
+
+// for(let i=0; i<radios.length; i++) {
+//     radios[i].addEventListener("change", () => {
+//         console.log(radios[i].value);
+//     })
+    // radios[i].onchange = function() {
+    //     console.log(radios[i].value);
+    // }
+// }
