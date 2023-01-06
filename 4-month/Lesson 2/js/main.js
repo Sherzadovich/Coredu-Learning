8c7876ee700dfd0c8ee63b499aa5602701345640
Sherzadovich <ibrohimjalilov2019@gmail.com>
2023-01-06 05:56:21 +0500

    forEach vs map

--- 4-month/Lesson 2/js/main.js
+++ 4-month/Lesson 2/js/main.js
@@ -38,3 +38,31 @@
 // looper(arr2, (e, i) => {
 //     console.log(e * 2);
 // });
+
+//? Array methods => Higher order functions
+// const arr = ["Ibrohim", "Sunnatilla", "Abbos", "Akbar"];
+
+// arr.forEach((e, i, array) => {
+//     console.log(e, i);
+// });
+
+//? Map => Array ichida o'zgarish bo'gandan keyin yengi array ochib beradi
+// const arr = ["Ibrohim", "Sunnatilla", "Abbos", "Akbar"];
+
+// const newArr = arr.map((element, index, arr) => element + " " + "Jalilov");
+// console.log(newArr);
+
+// function maper(arr, cb) {
+//     const result = [];
+//     for(let i=0; i<arr.length; i++) {
+//         const returnedData = cb(arr[i], i);
+//         result.push(returnedData);
+//     }
+//     return result;
+// }
+
+// const newArray = maper(arr, (e, i) => {
+//     return e + " " + "Jalilov"
+// });
+
+// console.log(newArray);
