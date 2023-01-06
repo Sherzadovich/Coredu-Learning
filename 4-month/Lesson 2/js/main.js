210fc1a0bd8f9eeaf56d55ba10b634f7bb82f381
Sherzadovich <ibrohimjalilov2019@gmail.com>
2023-01-06 05:18:41 +0500

    Higher order functions

--- 4-month/Lesson 2/js/main.js
+++ 4-month/Lesson 2/js/main.js
@@ -9,3 +9,32 @@
     //     console.log(radios[i].value);
     // }
 // }
+
+
+// ? Higher order functions
+// const radios = document.getElementsByName("gender");
+
+
+// function looper(arr, callback) {
+//     for(let i=0; i<arr.length; i++) {
+//         callback(arr[i], i);
+//     }
+// }
+
+// looper(radios, (radio) => {
+//     radio.addEventListener("change", () => {
+//         console.log(radio.value);
+//     })
+// });
+
+// const arr = ["Ibrohim", "Sunnatilla", "Abbos", "Akbar"];
+
+// looper(arr, (element, index) => {
+//     console.log(element, index);
+// });
+
+// const arr2 = [1,2,3,4,5,6,7,8];
+
+// looper(arr2, (e, i) => {
+//     console.log(e * 2);
+// });
