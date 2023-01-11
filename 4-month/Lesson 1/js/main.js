8d6d7b105f39ca9552bc64f01e1ad1dcbebe30c1
Sherzadovich <ibrohimjalilov2019@gmail.com>
2023-01-12 01:19:09 +0500

    Date objects

---
+++ 4-month/Lesson 1/js/main.js
@@ -0,0 +1,11 @@
+//? Date objects
+// const date = new Date();
+// console.log(date.getDay());   // Day
+// console.log(date.getDate());  // Date
+// const arr = ["January","February","March","April","May","June","July",
+// "August","September","October","November","December"]
+// console.log(arr[date.getMonth()]); // Month
+// console.log(date.getFullYear());
+// console.log(date.getHours());
+// console.log(date.getMinutes());
+// console.log(date.getSeconds());
