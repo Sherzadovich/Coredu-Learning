10f1d99c16bfdc40cd010df6de267173755154a7
Sherzadovich <ibrohimjalilov2019@gmail.com>
2023-01-12 21:13:43 +0500

    This

--- 4-month/Lesson 4/js/main.js
+++ 4-month/Lesson 4/js/main.js
@@ -6,3 +6,23 @@
 // const sortedArr = arr.sort((a,b) => a-b);
 // console.log(sortedUsers);
 // console.log(sortedArr);
+
+//? This
+
+// const user = {
+//     name: "Ibrohim",
+//     surname: "Jalilov",
+//     age: 20,
+//     sayFullName() {
+//         return this.name + " " + this.surname; 
+//     }
+// }
+
+// const arr = [
+//     function(firstname, surname) {
+//     return firstname + " " + surname;
+// }
+// ];
+// console.log(arr[0]("Ibrohim", "Jalilov"));
+
+// console.log(user.sayFullName());
