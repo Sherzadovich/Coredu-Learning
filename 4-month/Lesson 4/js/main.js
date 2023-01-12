46836960792554f7ded3002a109c1ab38d2d03bb
Sherzadovich <ibrohimjalilov2019@gmail.com>
2023-01-12 20:53:24 +0500

    sort

---
+++ 4-month/Lesson 4/js/main.js
@@ -0,0 +1,8 @@
+//? Sort
+// const arr = [1,5,90,63,2,45,5];
+// const users = ["Ibrohim", "Jalol", "Abdurahmon", "Fazliddin", "Kamol"];
+
+// const sortedUsers = users.sort();
+// const sortedArr = arr.sort((a,b) => a-b);
+// console.log(sortedUsers);
+// console.log(sortedArr);
