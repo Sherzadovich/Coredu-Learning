cec55f5de5afcede0a83e57c67940f684dbf4854
Sherzadovich <ibrohimjalilov2019@gmail.com>
2023-01-17 00:27:22 +0500

    Amaliyot

--- 4-month/Lesson 6/js/main.js
+++ 4-month/Lesson 6/js/main.js
@@ -16,3 +16,24 @@
 // const parsedData = JSON.parse(localStorage.getItem("myUser"));
 
 // console.log(parsedData);
+
+//? Amaliyot
+// const formEl = document.querySelector("#form");
+// const userContainer = document.querySelector("#users");
+
+// const usersList = JSON.parse(localStorage.getItem("users")) || [];
+
+// render();
+
+// formEl.addEventListener("submit", (e) => {
+//     e.preventDefault();
+//     usersList.push(e.target[0].value);
+//     render();
+// });
+
+// function render() {
+//     if(usersList.length > 0) {
+//         localStorage.setItem("users", JSON.stringify(usersList))
+//     }
+//         userContainer.textContent = usersList.join(", ");
+// }
