2e3f9ee1bfe1d884ef62813657acaf03e4867860
Sherzadovich <ibrohimjalilov2019@gmail.com>
2023-01-16 23:37:45 +0500

    JSON JavaScript Object Notation

---
+++ 4-month/Lesson 6/js/main.js
@@ -0,0 +1,13 @@
+//? JSON => JavaScript Object Notation
+const user = {
+    name: "Ibrohim",
+    age: 20,
+    isMarried: false
+};
+
+const jsonUser = JSON.stringify(user); // JSON format  {"name":"Ibrohim","age":20,"isMarried":false}
+// console.log(jsonUser);
+// const parsedUser = JSON.parse(jsonUser); // object format {name: 'Ibrohim', age: 20, isMarried: false}
+// console.log(parsedUser);
+
+//? Storages => localStorage, Cookie Storage, session storage
