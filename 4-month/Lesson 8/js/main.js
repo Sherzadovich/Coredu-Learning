f0a646b21f7a92b41e8960e4edeab35b73f9eb82
Sherzadovich <ibrohimjalilov2019@gmail.com>
2023-01-21 20:37:46 +0500

    Promise => Pending, Fullfilled and Reject

--- 4-month/Lesson 8/js/main.js
+++ 4-month/Lesson 8/js/main.js
@@ -10,3 +10,24 @@
 // console.log(3);
 
 // myFunc();
+
+//? Promise => Pending, Fullfilled and Reject
+// const myPromise = new Promise((resolve, reject) => {
+//     if(2===2) {
+//         setTimeout(() => {
+//             resolve({name: "Ibrohim"});
+//         }, 2000);
+//     } else {
+//         reject("Error")
+//     }
+// });
+
+// // console.log(myPromise);
+
+// myPromise.then((response) => {
+//     console.log(response);
+// }).catch((err) => {
+//     console.log(err);
+// }).finally(console.log("Done"));
+
+// console.log("Hello 2");
