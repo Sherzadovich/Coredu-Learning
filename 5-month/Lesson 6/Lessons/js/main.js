859cc66e38eef0362c6a6c8486e5c9a5ee4ae506
Sherzadovich <ibrohimjalilov2019@gmail.com>
2023-02-12 17:11:57 +0500

    Object.create()

--- 5-month/Lesson 6/Lessons/js/main.js
+++ 5-month/Lesson 6/Lessons/js/main.js
@@ -1,28 +1,28 @@
 //? Object.create()
 
-const PersonPrototype = {
-    init(name, bithYear) {
-        this.name = name;
-        this.bithYear = bithYear;
-    },
+// const PersonPrototype = {
+//     init(name, bithYear) {
+//         this.name = name;
+//         this.bithYear = bithYear;
+//     },
 
-    calcAge() {
-        return 2023 - this.bithYear;
-    },
-}
+//     calcAge() {
+//         return 2023 - this.bithYear;
+//     },
+// }
 
-const jay = Object.create(PersonPrototype);
-jay.init("Jay", 2000);
+// const jay = Object.create(PersonPrototype);
+// jay.init("Jay", 2000);
 
 // jay.name = " Jay";
 // jay.bithYear = 2000;
 
-console.log(jay.calcAge());
-console.log(jay);
+// console.log(jay.calcAge());
+// console.log(jay);
 
-const Ibrohim = Object.create(PersonPrototype)
+// const Ibrohim = Object.create(PersonPrototype)
 
-Ibrohim.init("Ibrohim", 2002);
+// Ibrohim.init("Ibrohim", 2002);
 
-console.log(Ibrohim);
-console.log(Ibrohim.calcAge());
+// console.log(Ibrohim);
+// console.log(Ibrohim.calcAge());
