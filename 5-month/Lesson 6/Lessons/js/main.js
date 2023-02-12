bd43cca9d366ccc53cfc6b88b871bb536e736474
Sherzadovich <ibrohimjalilov2019@gmail.com>
2023-02-12 17:25:04 +0500

    Inheritence in Object.create()

--- 5-month/Lesson 6/Lessons/js/main.js
+++ 5-month/Lesson 6/Lessons/js/main.js
@@ -26,3 +26,32 @@
 
 // console.log(Ibrohim);
 // console.log(Ibrohim.calcAge());
+
+//? Inheritence in Object.create()
+
+// const PersonPrototype = {
+//     init(name, bithYear) {
+//         this.name = name;
+//         this.bithYear = bithYear;
+//         return this
+//     },
+
+//     calcAge() {
+//         return 2023 - this.bithYear;
+//     },
+// }
+
+// const jay = Object.create(PersonPrototype).init("Jay", 2005);
+
+// const StudentPrototype = Object.create(PersonPrototype);
+
+// StudentPrototype.init = function(name, bithYear, course) {
+//     PersonPrototype.init.call(this, name, bithYear);
+//     this.course = course;
+// }
+
+// const harry = Object.create(StudentPrototype)
+// harry.init("Harry", 2009, "Math")
+
+// console.log(harry.calcAge());
+// console.log(harry);
