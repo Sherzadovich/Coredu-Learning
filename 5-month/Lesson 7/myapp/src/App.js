bc5e8d0f138644a84d40ba5aea9b7ca7f2cdc394
Sherzadovich <ibrohimjalilov2019@gmail.com>
2023-02-14 23:24:46 +0500

    Styling in react

--- 5-month/Lesson 7/myapp/src/App.js
+++ 5-month/Lesson 7/myapp/src/App.js
@@ -1,11 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const h1style = {
+    color: "red",
+    backgroundColor: "black"
+  }
+
+  //* class = className
+  //* for = htmlFor
+
   return (
     <div className="App">
-      <h1>Bolla hammaga salom!</h1>
-      
+      <h1 style={h1style}>Bolla hammaga salom!</h1>
     </div>
   );
 }
