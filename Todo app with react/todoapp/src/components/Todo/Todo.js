c480e5e8678d9b2369e29ce538cd0f056855a7ee
Sherzadovich <ibrohimjalilov2019@gmail.com>
2023-03-05 23:59:28 +0500

    Todo app

--- Todo app with react/todoapp/src/components/Todo/Todo.js
+++ Todo app with react/todoapp/src/components/Todo/Todo.js
@@ -1,13 +1,41 @@
-import React from "react";
+import React, { useRef } from "react";
+import { render } from "../TodoList/TodoList";
 import "../Todo/Todo.css";
 
 export default function Todo() {
+  const formRef = useRef();
+
+  let todo = JSON.parse(localStorage.getItem("User")) || [];
+
+  const ref = (event) => {
+    event.preventDefault();
+    const task = event.target[0].value;
+
+    const todoObj = {
+      id: todo.length,
+      task: task,
+      isCompleted: false,
+      isEditing: false,
+    };
+
+    todo.push(todoObj);
+    localStorage.setItem("User", JSON.stringify(todo));
+    console.log(todo);
+  };
+
   return (
     <div className="todo">
       <h1>Todo App</h1>
-      <form>
+      <form id="form" ref={formRef} onSubmit={ref}>
         <input type="text" />
-        <button className="btn">Add Todo</button>
+        <button
+          className="btn"
+          onClick={render("Ibrohim", 2002, function (myTemplate) {
+            console.log(myTemplate);
+          })}
+        >
+          Add Todo
+        </button>
       </form>
     </div>
   );
--- Todo app with react/todoapp/src/components/TodoList/TodoList.js
+++ Todo app with react/todoapp/src/components/TodoList/TodoList.js
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { useRef } from "react";
 import "../TodoList/TodoList.css";
 
+function render(firstName, birthYear, callback) {
+  const currentYear = new Date().getFullYear();
+  const age = currentYear - Number(birthYear);
+  const template = `${firstName} bu yil ${age} yoshda!`;
+  callback(template);
+}
+
 export default function TodoList() {
+  const cardRef = useRef();
+  // render("Ibrohim", 2002, function (myTemplate) {
+  //   console.log(myTemplate);
+  // });
   return (
-    <div className="container">
+    <div className="cards" ref={cardRef}>
       <div className="card">
         <div className="form_input">
           <form className="form">
             <input type="checkbox" id="todoInput" />
-            <label htmlFor="todoInput">qwrew</label>
+            <label htmlFor="todoInput">Hello</label>
           </form>
         </div>
         <div className="form_button">
@@ -19,3 +30,5 @@ export default function TodoList() {
     </div>
   );
 }
+
+export { render };
